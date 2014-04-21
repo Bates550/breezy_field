a3347a9696522ec35999c67b1f960d69d8a67e57
Sean Spearman <seanmspearman@gmail.com>
2014-04-20 23:25:58 -0700

    Added map wrapping. Refactored some stuff.
    - If going any farther would cause the camera view to be outside the game world's bounds, move lucas to the opposite end of the game world. This essentially allows the map to loop.
    - There is an issue with moving the player to a position that is still in sync with the background grass. With current camera and world bounds and the wrap_overshoot var, map wrapping looks pretty smooth, but these are hard coded and it would be nice to calculate the wrap_overshoot as a function of camera and world bounds and perhaps the size of the background tile.

--- js/game.js
+++ js/game.js
@@ -1,29 +1,45 @@
-var game = new Phaser.Game(800, 600, Phaser.CANVAS, '', { preload: preload, create: create, update: update });
+var window_width = document.documentElement.clientWidth; //window.screen.availWidth;
+var window_height = document.documentElement.clientHeight; //window.screen.availHeight;
+var game = new Phaser.Game(600, 600, Phaser.CANVAS, '', { preload: preload, create: create, update: update });
 
 function preload() {
 	game.load.image('grass', 'assets/grass_tile.png');
 	game.load.spritesheet('lucas_walk', 'assets/lucas_walk.png', 18, 28);
-
 }
 
 var lucas = {};
 var grass;
+var cursors; 
 
 function create() {
+	var world_bounds = { x: 0, y: 0, w: 1421, h: 1421 };
+	/*var camera_bounds = {	x: world_bounds.x-game.width/2,
+							y: world_bounds.y-game.height/2,
+							w: world_bounds.w+game.height/2,
+							h: world_bounds.h+game.height/2 };
+	*/
+	game.world.setBounds(world_bounds.x, world_bounds.y, world_bounds.w, world_bounds.h);
+	//game.camera.bounds = new Phaser.Rectangle(camera_bounds.x, camera_bounds.y, camera_bounds.w, camera_bounds.h);
+
+	/* Keyboard Input */ 
+	cursors = game.input.keyboard.createCursorKeys();
+
 	/* Grass */
-	grass = game.add.tileSprite(0, 0, game.width, game.height, 'grass');
+	grass = game.add.tileSprite(game.world.x, game.world.y, game.world.width, game.world.height, 'grass');
 	grass.scale.setTo(2, 2);
 
 	/* Lucas */
-	lucas.current_direction = 'down';
+	lucas.current_direction = 'none';
 	lucas.update_threshold = 7;
 	lucas.last_updated = 0;
 	lucas.speed = 2;
 	lucas.scale = 2;
 	lucas.fps = 6;
 
-	lucas.walk = game.add.sprite(game.width/2, game.height/2, 'lucas_walk');
+	lucas.walk = game.add.sprite(game.width/2+50, game.height/2+50, 'lucas_walk');
 	lucas.walk.scale.setTo(lucas.scale, lucas.scale);
+	lucas.walk.anchor.setTo(0.5, 0.5);
+	game.camera.follow(lucas.walk);
 
 	lucas.walk.animations.add('down-left', 	[4, 3, 5, 3], 		lucas.fps, true);
 	lucas.walk.animations.add('down', 		[1, 0, 2, 0], 		lucas.fps, true);
@@ -66,30 +82,53 @@ function create() {
 
 function update() {
 	var kb_in = kb_input();
+	update_lucas(kb_in);
+}
 
+function update_lucas(kb_in) {
 	lucas.last_updated++;
+	var update_direction = false;
+	var wrap_overshoot = 36;
+
+	/* Map wrapping */
+	// If going farther would cause the camera to display something outside of the game world
+	// put lucas on the opposite side of the world such that it loops (and there is no escaping!)
+	if (lucas.walk.x <= (game.world.bounds.x+game.width/2) ) {
+		console.log(lucas.walk.x, game.world.bounds.x);
+		lucas.walk.x = game.world.bounds.width-game.width/2-wrap_overshoot;
+	}
+	if (lucas.walk.x >= (game.world.bounds.width-game.width/2) ) {
+		console.log(lucas.walk.x, game.world.bounds.width);
+		lucas.walk.x = game.world.bounds.x+game.width/2+wrap_overshoot;
+	}
+	if (lucas.walk.y <= game.world.bounds.y+game.height/2) {
+		console.log(lucas.walk.y, game.world.bounds.y);
+		lucas.walk.y = game.world.bounds.height-game.height/2-wrap_overshoot;
+	}
+	if (lucas.walk.y >= game.world.bounds.height-game.height/2) {
+		console.log(lucas.walk.y, game.world.bounds.height);
+		lucas.walk.y = game.world.bounds.y+game.height/2+wrap_overshoot;
+	}
+
 	// Potential error condition if a player moves in the same direction for a REALLY long time (like 4.8 million years)
 	// 9007199254740992*(1/60)*(1/3600)*(1/24)*(1/7)*(1/52)
 	// ^-max safe int-^^1/fps^ ^-s/h-^  ^h/dy^^dy/wk^^wk/yr^
-	// Not realistic but going in one direction for 4.8 million years is amusing so I'm gonna leave this here. 
 	if (lucas.last_updated == Number.MAX_SAFE_INTEGER) {
 		lucas.last_updated = 0;
 	}
-	var update = false;
-
 
 	// If keyboard input is in a different direction than the current direction 
-	// AND the object has not been recently updated
-	// update the position and the animation
+	// AND the object has not been recently updated:
+	// update the position, current direction, and animation
 	// Else update only the position
 	if ( (kb_in != lucas.current_direction) && (lucas.last_updated % lucas.update_threshold == 0) ) {
-		update = true;
+		update_direction = true;
 		lucas.last_updated = 0;
 	}
 	if (kb_in == 'down-left') {
 		lucas.walk.x -= lucas.speed;
 		lucas.walk.y += lucas.speed;
-		if (update) {
+		if (update_direction) {
 			lucas.current_direction = 'down-left';
 			lucas.walk.animations.play('down-left');
 		}
@@ -97,7 +136,7 @@ function update() {
 	else if (kb_in == 'down-right') {
 		lucas.walk.x += lucas.speed;
 		lucas.walk.y += lucas.speed;
-		if (update) {
+		if (update_direction) {
 			lucas.current_direction = 'down-right';
 			lucas.walk.animations.play('down-right');
 		}
@@ -105,7 +144,7 @@ function update() {
 	else if (kb_in == 'up-left') {
 		lucas.walk.x -= lucas.speed;
 		lucas.walk.y -= lucas.speed;
-		if (update) {	
+		if (update_direction) {	
 			lucas.current_direction = 'up-left';
 			lucas.walk.animations.play('up-left');
 		}
@@ -113,35 +152,35 @@ function update() {
 	else if (kb_in == 'up-right') {
 		lucas.walk.x += lucas.speed;
 		lucas.walk.y -= lucas.speed;
-		if (update) {	
+		if (update_direction) {	
 			lucas.current_direction = 'up-right';
 			lucas.walk.animations.play('up-right');
 		}
 	}
 	else if (kb_in == 'down') {
 		lucas.walk.y += lucas.speed;
-		if (update) {	
+		if (update_direction) {	
 			lucas.current_direction = 'down';
 			lucas.walk.animations.play('down');
 		}
 	}
 	else if (kb_in == 'up') {
 		lucas.walk.y -= lucas.speed;
-		if (update) {
+		if (update_direction) {
 			lucas.current_direction = 'up';
 			lucas.walk.animations.play('up');
 		}
 	}
 	else if (kb_in == 'left') {
 		lucas.walk.x -= lucas.speed;
-		if (update) {	
+		if (update_direction) {	
 			lucas.current_direction = 'left';
 			lucas.walk.animations.play('left');
 		}
 	}
 	else if (kb_in == 'right') {
 		lucas.walk.x += lucas.speed;
-		if (update) {	
+		if (update_direction) {	
 			lucas.current_direction = 'right';
 			lucas.walk.animations.play('right');
 		}
@@ -154,10 +193,10 @@ function update() {
 // If both left and right or both up and down are pressed kb_input will return a nonsense value. 
 function kb_input() {
 	var result 	= "";
-	var up 		= game.input.keyboard.isDown(Phaser.Keyboard.UP);
-	var right 	= game.input.keyboard.isDown(Phaser.Keyboard.RIGHT);
-	var down 	= game.input.keyboard.isDown(Phaser.Keyboard.DOWN);
-	var left 	= game.input.keyboard.isDown(Phaser.Keyboard.LEFT);
+	var right 	= cursors.right.isDown;	//game.input.keyboard.isDown(Phaser.Keyboard.RIGHT);
+	var left 	= cursors.left.isDown;	//game.input.keyboard.isDown(Phaser.Keyboard.LEFT);
+	var down 	= cursors.down.isDown;	//game.input.keyboard.isDown(Phaser.Keyboard.DOWN);
+	var up 		= cursors.up.isDown;	//game.input.keyboard.isDown(Phaser.Keyboard.UP);
 
 	if (up) {
 		if (result !== "") {
